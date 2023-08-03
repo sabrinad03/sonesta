06090e540b87603d12b0120daf65b74fdf047861
sabrinad03 <115737894+sabrinad03@users.noreply.github.com>
2023-08-04 02:40:29 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function viewClocks() {
+		const myDiv = document.getElementById('additional-clocks');
+		const myBtn = document.getElementById('btn-clock');
+		myDiv.classList.toggle('d-none');
+		myBtn.classList.toggle('active');
+	}
